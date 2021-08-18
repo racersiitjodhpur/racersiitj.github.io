b9317c2e04aa2350e8a85d081cec254f5068b84e
racersiitjodhpur <30953761+racersiitjodhpur@users.noreply.github.com>
2021-08-18 12:21:45 +0530

    Delete mail2.php

--- mail2.php
+++
@@ -1,7 +0,0 @@
-<?php $email = $_POST['email'];
-$recipient = "das.3@iitj.ac.in";
-$subject = "Subscribe";
-$mailheader = "From: $email \r\n";
-mail($recipient, $mailheader) or die("Error!");
-echo "Thank You!";
-?>
